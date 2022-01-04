cd4191a5f27115f4073c6496620372d048623cf7
abdo2 <abdopetroleum>
2022-01-04 18:50:14 +0200

    New commit

--- setup.py
+++ setup.py
@@ -1,31 +1,14 @@
 from distutils.core import setup
+
 setup(
-  name = 'BeggsandBrill',
-  packages = ['BeggsandBrill'],
-  version = '0.2',
-  license='MIT',
-  description = 'Production System Analysis',
-  author = 'Orkahub Energy',
-  author_email = 'orkahub@gmail.com',
-  url = 'https://github.com/orkahub/production_systems_analysis',
-  download_url = 'https://github.com/orkahub/production_systems_analysis/archive/v_02.tar.gz',
-  keywords = ['RESERVOIR', 'ENERGY', 'OIL', 'GAS', 'PRODUCTION', 'VLP', 'IPR'],
-  install_requires=[
-          'pandas',
-          'numpy',
-          'scipy',
-          'seaborn',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-  ],
+  name = 'ShahienBeggs',
+  packages = ['ShahienBeggs'],
+  version = 'version number',  # Ideally should be same as your GitHub release tag varsion
+  description = 'description',
+  author = 'Abdulrhman Shahien',
+  author_email = '',
+  url = 'github package source url',
+  download_url = 'download link you saved',
+  keywords = ['tag1', 'tag2'],
+  classifiers = [],
 )
