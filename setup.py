571d3225194c039339e79c79f5ab2cf4c37deec1
abdo2 <abdopetroleum>
2022-01-06 16:53:36 +0200

    New commit

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from distutils.core import setup
 
 setup(
-  name = 'ShahienBeggs',
-  packages = ['ShahienBeggs'],
+  name = 'shahienetal',
+  packages = ['shahienetal'],
   version = '1',  # Ideally should be same as your GitHub release tag varsion
   description = 'description',
   license='MIT',
   author = 'Abdulrhman Shahien',
   author_email = 'abdopetroleum@gmail.com',
   url = 'https://github.com/abdopetroleum/ShahienBeggs',
-  download_url = 'https://github.com/abdopetroleum/ShahienBeggs/archive/refs/tags/1.tar.gz',
   install_requires=[
     'pandas',
     'numpy',
